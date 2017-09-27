c618a788cd631bc874986ccd4de471555caaa876
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2017-09-27 19:19:25 +0100

    use collections template (with table)

--- static/js/crowd.js
+++ static/js/crowd.js
@@ -14,8 +14,168 @@ console.log("SERVERBASE: ",serverbase);
 $(document).ready(function(){
 
 	init_thumbs();
+	init_collections_table();
+	init_documents_table();
 	init_subscription();
 });
+
+function init_collections_table(){
+
+	if(!$("#collections_table").length) return;
+
+	var url = make_url("/utils/table_ajax_public/crowdsourcing");
+
+	var columns =  [
+		    { "data" : null,
+		      "defaultContent": '<span class="glyphicon glyphicon-refresh glyphicon-spin"></span>', 
+		      "searchable": false, 
+		      "orderable": false},
+		    { "data": "colName" },
+		    { "data": "nrOfDocuments" },
+        	];
+	console.log("Let's go");
+	var datatable = init_datatable($("#collections_table"),url,columns);
+	console.log("apres datatable");
+
+	$("#collections_table").on( 'draw.dt', function () {
+		console.log("datatable been drewed");
+
+	    var api = new $.fn.DataTable.Api( "#collections_table" );
+		row_data = [];
+		//rowIdx is the index of this row before the sort/search, rowLoop contains the (current) idx of the row after the sort
+		api.rows({page:'current'}).every( function ( rowIdx, tableLoop, rowLoop ) {
+
+			var d = this.data();
+		    this.invalidate('dom');
+		    var currRow = this;
+
+			$("#collections_table tbody tr").each(function(rowInd){
+		    	if (rowLoop == rowInd){
+			/*		 
+					row_data[rowInd] = {} ;
+					row_data[rowInd].collId = currRow.data().colId;
+					row_data[rowInd].url = make_url("/utils/thumb/"+row_data[rowInd].collId);
+					row_data[rowInd].collStat = make_url("/library/coll_statistics/"+row_data[rowInd].collId);
+
+					row_data[rowInd].img_cell = this;
+					$.getJSON(row_data[rowInd].url, function(thumb_data){
+						if (thumb_data.url){
+							thumb = loadThumbs(thumb_data.url);
+							$("td:eq(0)", row_data[rowInd].img_cell).empty();
+							$("td:eq(0)", row_data[rowInd].img_cell).append(thumb);
+							$("td:eq(0)", row_data[rowInd].img_cell).addClass('text-center');
+						}else{
+							$("td:eq(0)", row_data[rowInd].img_cell).html('No image available');
+						}
+					}).done(function(a,b) {
+					    console.log( "Done: ",a, " ",b );
+					}).fail(function( a, b){
+					    console.log( "Fail: ",a, " ",b );
+					});
+
+					row_data[rowInd].collStat = make_url("/library/coll_statistics/"+row_data[rowInd].collId);
+					$.getJSON(row_data[rowInd].collStat, function(stat_data){
+						$("td:eq(2)", row_data[rowInd].img_cell).html(stat_data.titleDesc);
+						shorten_text("long_text_" + row_data[rowInd].collId);
+					}).done(function(a,b) {
+					    console.log( "Done: ",a, " ",b );
+					}).fail(function( a, b){
+					    console.log( "Fail: ",a, " ",b );
+					});
+			*/
+		    	}
+			});
+		});
+	});
+
+}
+
+function init_documents_table(){
+
+	if(!$("#documents_table").length) return;
+
+//	var url = "./table_ajax/documents/"+window.location.pathname.replace(/^.*\/(\d+)$/, '$1');
+	var url = make_url("/utils/table_ajax/documents/"+window.location.pathname.replace(/^.*\/(\d+)$/, '$1'));
+
+	var ids = parse_path();	
+
+	var columns =  [
+		    //This column will be for our image which we will not get from the table_ajax/document view
+		    { "data": "docId",
+		      "defaultContent": '<span class="glyphicon glyphicon-refresh glyphicon-spin"></span>'},
+		    { "data" : null, 
+		      "searchable": false, 
+		      "orderable": false},
+		    { "data": null, 
+		      "defaultContent": '<span class="glyphicon glyphicon-refresh glyphicon-spin"></span>',
+		      "searchable": false, 
+		      "orderable": false},
+		    { "data": null,
+		      "searchable": false },
+		    { "data" : "nrOfPages", 
+		      "searchable": false, 
+		      "orderable": false}
+		    
+		    //{ "data": "new_key" },//if we want to add a new column in this table
+        	];
+
+	var datatable = init_datatable($("#documents_table"),url,columns);
+			
+	//redraw handled here: may an easier solution to get current docID after a sort/search exist but this one works fine for the moment
+	$("#documents_table ").on( 'draw.dt', function () {
+		
+	    var api = new $.fn.DataTable.Api( "#documents_table" );
+		row_data = [];
+		//rowIdx is the index of this row before the sort/search, rowLoop contains the (current) idx of the row after the sort
+		api.rows({page:'current'}).every( function ( rowIdx, tableLoop, rowLoop ) {
+
+			var d = this.data();
+		    this.invalidate('dom');
+		    var currRow = this;
+		    
+/*		    console.log( "rowIdx: ", rowIdx);
+		    console.log( "rowLoop: ", rowLoop);
+		    console.log( "currRow: ", currRow.data().docId);*/
+		    
+		    //this way we can only go throug the indizes of the first page before the change during sort/search
+		    $("#documents_table tbody tr").each(function(rowInd){
+		    	
+		    	//console.log( "rowInd: ", rowInd);
+		    	if (rowLoop == rowInd){
+		    				 
+					row_data[rowInd] = {} ;
+					row_data[rowInd].docId = currRow.data().docId;
+					row_data[rowInd].url = make_url("/utils/thumb/"+ids['collId']+'/'+row_data[rowInd].docId);
+					row_data[rowInd].img_cell = this;
+					$.getJSON(row_data[rowInd].url, function(thumb_data){
+						thumb = loadThumbs(thumb_data.url);
+						$("td:eq(0)", row_data[rowInd].img_cell).empty();
+						$("td:eq(0)", row_data[rowInd].img_cell).append(thumb);
+						$("td:eq(0)", row_data[rowInd].img_cell).addClass('text-center');
+					}).done(function(a,b) {
+					    console.log( "Done: ",a, " ",b );
+					}).fail(function( a, b){
+					    console.log( "Fail: ",a, " ",b );
+					});
+
+					row_data[rowInd].stats = make_url("/library/statistics/"+ids['collId']+'/'+row_data[rowInd].docId);
+					$.getJSON(row_data[rowInd].stats, function(stat_data){
+						$("td:eq(2)", row_data[rowInd].img_cell).html(stat_data.titleDesc);
+						$("td:eq(3)", row_data[rowInd].img_cell).html(stat_data.statString);
+						$("td:eq(1)", row_data[rowInd].img_cell).html(stat_data.viewLinks);
+
+						shorten_text("long_text_" + row_data[rowInd].docId);
+					}).done(function(a,b) {
+					    console.log( "Done: ",a, " ",b );
+					}).fail(function( a, b){
+					    console.log( "Fail: ",a, " ",b );
+					});
+			    }
+		    });
+		});
+	});
+}
+
 
 function init_subscription(){
 
